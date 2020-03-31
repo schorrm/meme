be95ad41acb58b5382be74a78fdde0a59254212c
Lior Ramati <firerogue517@gmail.com>
2020-03-31 13:20:01 +0300

    Set up default field config

--- src/defines.py
+++ src/defines.py
@@ -9,7 +9,18 @@ DEFAULT_SIZE = (640, 480)
 SML_DIR = '/usr/local/lib/meme/sml'
 LIB_DIR = '/usr/local/lib/meme/lib'
 
-DEFAULT_FIELD_CFG = {} # TODO : DEFINE
+DEFAULT_FIELD_CFG = { "RIGHT"  : ("50%",  "0%", "100%", "100%"),
+                      "LEFT"   : ( "0%",  "0%",  "50%", "100%"),
+                      "top"    : ( "0%",  "0%", "100%",  "20%"),
+                      "bottom" : ( "0%", "80%", "100%", "100%"),
+                      "center" : ( "0%", "40%", "100%",  "60%"),
+                      "rtop"   : ("50%",  "0%", "100%",  "20%"),
+                      "rbottom": ("50%", "80%", "100%", "100%"),
+                      "rcenter": ("50%", "40%", "100%",  "60%"),
+                      "ltop"   : ( "0%",  "0%",  "50%",  "20%"),
+                      "lbottom": ( "0%", "80%",  "50%", "100%"),
+                      "lcenter": ( "0%", "40%",  "50%",  "60%")
+                    } # TODO : DEFINE
 
 CONFIG_EXT = '.memeconfig' # My suggestion
 
