7d753c76982d93fed7ea8bf16ff1c337531f62ec
Moshe Schorr <mosheraphael@gmail.com>
2020-04-01 23:24:56 +0300

    Manifest, setup for release

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='meme',
+    version='0.1.0',
+    description='Standard Meme Compiler v. 0.1',
+    author='Lior Ramati & Moshe Schorr',
+    url='https://github.com/schorrm/meme',
+    packages=find_packages(include=['src', 'src/*']),
+    include_package_data=True,
+    license='LICENSE',
+    install_requires=[
+        'Pillow'
+    ],
+    entry_points={
+        'console_scripts': ['meme=src.console:main']
+    },
+)
